2beb7931d883b944c85bdb60f24f2a021548e088
YeLuo <81338824+YeluoRain@users.noreply.github.com>
2025-11-10 22:51:05 +0800

    improve code index

--- summer/Summer.h
+++ summer/Summer.h
@@ -10,72 +10,125 @@
 namespace summer {
 namespace hana = boost::hana;
 
+/**
+ * @brief BeanFactory is used to access beans created by bean creators.
+ *
+ * @tparam AllBeanContainer A Boost.Hana tuple containing all bean creators and bean types.
+ */
 template <typename AllBeanContainer>
 class BeanFactory {
   public:
+    /**
+     * @brief Get the Shared object from the factory.
+     *
+     * @tparam BeanType The type of the bean to retrieve.
+     * @return std::shared_ptr<BeanType> The shared pointer to the bean instance.
+     */
     template <typename BeanType>
     std::shared_ptr<BeanType> GetShared() {
-        static_assert(hana::contains(container, hana::type_c<BeanType>),
-                      "BeanType not found in BeanFactory");
-        auto creators = container[hana::type_c<BeanType>];
+        auto creators = FindCreators<BeanType>();
         static_assert(hana::size(creators) == hana::size_c<1>,
                       "There should be only one creator for a bean type.");
         return creators[hana::size_c<0>].GetShared();
     }
 
+    /**
+     * @brief Get the Unique object from the factory.
+     *
+     * @tparam BeanType The type of the bean to retrieve.
+     * @return std::unique_ptr<BeanType> The unique pointer to the bean instance.
+     */
     template <typename BeanType>
     std::unique_ptr<BeanType> GetUnique() {
-        static_assert(hana::contains(container, hana::type_c<BeanType>),
-                      "BeanType not found in BeanFactory");
-        auto creators = container[hana::type_c<BeanType>];
+        auto creators = FindCreators<BeanType>();
         static_assert(hana::size(creators) == hana::size_c<1>,
                       "There should be only one creator for a bean type.");
         return creators[hana::size_c<0>].GetUnique();
     }
 
+    /**
+     * @brief Get the Shared List object from the factory.
+     *
+     * @tparam BeanType The type of the bean to retrieve.
+     * @return std::list<std::shared_ptr<BeanType>> The list of shared pointers to the bean
+     * instances.
+     */
     template <typename BeanType>
     std::list<std::shared_ptr<BeanType>> GetSharedList() {
-        static_assert(hana::contains(container, hana::type_c<BeanType>),
-                      "BeanType not found in BeanFactory");
-        auto creators = container[hana::type_c<BeanType>];
+        auto creators = FindCreators<BeanType>();
         std::list<std::shared_ptr<BeanType>> result;
         hana::for_each(creators, [&](auto creator) { result.push_back(creator.GetShared()); });
         return result;
     }
 
+    /**
+     * @brief Get the Unique List object from the factory.
+     *
+     * @tparam BeanType The type of the bean to retrieve.
+     * @return std::list<std::unique_ptr<BeanType>> The list of unique pointers to the bean
+     * instances.
+     */
     template <typename BeanType>
     std::list<std::unique_ptr<BeanType>> GetUniqueList() {
-        static_assert(hana::contains(container, hana::type_c<BeanType>),
-                      "BeanType not found in BeanFactory");
-        auto creators = container[hana::type_c<BeanType>];
+        auto creators = FindCreators<BeanType>();
         std::list<std::unique_ptr<BeanType>> result;
         hana::for_each(creators, [&](auto creator) { result.push_back(creator.GetUnique()); });
         return result;
     }
 
   private:
+    template <typename BeanType>
+    auto FindCreators() {
+        static_assert(hana::contains(container, hana::type_c<BeanType>),
+                      "BeanType not found in BeanFactory");
+        return container[hana::type_c<BeanType>];
+    }
+
     inline static decltype(bean::factory::Beans::CreateFactory(AllBeanContainer{})) container =
         bean::factory::Beans::CreateFactory(AllBeanContainer{});
 };
 
+/**
+ * @brief Bean Factory Builder
+ *
+ * @tparam BeanContainer Container of bean types
+ * @tparam CreatorContainer Container of bean creators
+ */
 template <typename BeanContainer = decltype(hana::make_tuple()),
           typename CreatorContainer = decltype(hana::make_tuple())>
-class ContainerBuilder {
+class FactoryBuilder {
   public:
+    /**
+     * @brief Include bean types into the container
+     *
+     * @tparam BeanTypes Types of beans to include
+     * @return constexpr auto New FactoryBuilder with added bean types
+     */
     template <typename... BeanTypes>
     constexpr auto WithBeans() {
-        return ContainerBuilder<decltype(hana::concat(beanContainer, hana::tuple_t<BeanTypes...>)),
-                                CreatorContainer>{};
+        return FactoryBuilder<decltype(hana::concat(beanContainer, hana::tuple_t<BeanTypes...>)),
+                              CreatorContainer>{};
     }
 
+    /**
+     * @brief Include bean creators into the container
+     *
+     * @tparam Creators Types of bean creators to include
+     * @return constexpr auto New FactoryBuilder with added bean creators
+     */
     template <auto... Creators>
     constexpr auto WithCreators() {
-        return ContainerBuilder<BeanContainer,
-                                decltype(hana::concat(
-                                    creatorContainer,
-                                    hana::tuple_t<bean::traits::CreatorWrapper<Creators>...>))>{};
+        return FactoryBuilder<BeanContainer,
+                              decltype(hana::concat(
+                                  creatorContainer,
+                                  hana::tuple_t<bean::traits::CreatorWrapper<Creators>...>))>{};
     }
 
+    /**
+     * @brief Build the BeanFactory
+     *
+     * @return constexpr auto BeanFactory instance
+     */
     constexpr auto Build() {
         auto all = hana::concat(beanContainer, creatorContainer);
         return BeanFactory<decltype(all)>{};
--- test/BeanTest.cpp
+++ test/BeanTest.cpp
@@ -8,13 +8,13 @@ using namespace summer;
 
 TEST_F(BeanTest, test_construct_beans_by_bean_factory) {
     using namespace NormalCase;
-    auto container = ContainerBuilder<>().WithBeans<CImpl, BImpl, AImpl>().Build();
-    auto a = container.GetShared<A>();
-    auto b = container.GetShared<B>();
-    auto c = container.GetShared<C>();
-    auto a1 = container.GetShared<AImpl>();
-    auto b1 = container.GetShared<BImpl>();
-    auto c1 = container.GetShared<CImpl>();
+    auto factory = FactoryBuilder<>().WithBeans<CImpl, BImpl, AImpl>().Build();
+    auto a = factory.GetShared<A>();
+    auto b = factory.GetShared<B>();
+    auto c = factory.GetShared<C>();
+    auto a1 = factory.GetShared<AImpl>();
+    auto b1 = factory.GetShared<BImpl>();
+    auto c1 = factory.GetShared<CImpl>();
     EXPECT_EQ(a.get(), a1.get());
     EXPECT_EQ(b.get(), b1.get());
     EXPECT_EQ(c.get(), c1.get());
@@ -22,12 +22,12 @@ TEST_F(BeanTest, test_construct_beans_by_bean_factory) {
 
 TEST_F(BeanTest, test_construct_beans_with_creator_function) {
     using namespace NormalCase;
-    auto container = ContainerBuilder<>().WithBeans<BImpl, AImpl>().WithCreators<createC>().Build();
-    auto a = container.GetShared<A>();
-    auto b = container.GetShared<B>();
-    auto c = container.GetShared<C>();
-    auto a1 = container.GetShared<AImpl>();
-    auto b1 = container.GetShared<BImpl>();
+    auto factory = FactoryBuilder<>().WithBeans<BImpl, AImpl>().WithCreators<createC>().Build();
+    auto a = factory.GetShared<A>();
+    auto b = factory.GetShared<B>();
+    auto c = factory.GetShared<C>();
+    auto a1 = factory.GetShared<AImpl>();
+    auto b1 = factory.GetShared<BImpl>();
     // auto c1 = container.GetShared<CImpl>();
     EXPECT_EQ(a.get(), a1.get());
     EXPECT_EQ(b.get(), b1.get());
@@ -36,10 +36,10 @@ TEST_F(BeanTest, test_construct_beans_with_creator_function) {
 
 TEST_F(BeanTest, test_construct_all_beans_with_creator_function) {
     using namespace NormalCase;
-    auto container = ContainerBuilder<>().WithCreators<createC, createA, createB>().Build();
-    auto a = container.GetShared<A>();
-    auto b = container.GetShared<B>();
-    auto c = container.GetShared<C>();
+    auto factory = FactoryBuilder<>().WithCreators<createC, createA, createB>().Build();
+    auto a = factory.GetShared<A>();
+    auto b = factory.GetShared<B>();
+    auto c = factory.GetShared<C>();
     // auto a1 = container.GetShared<AImpl>();
     // auto b1 = container.GetShared<BImpl>();
     // auto c1 = container.GetShared<CImpl>();
@@ -50,11 +50,11 @@ TEST_F(BeanTest, test_construct_all_beans_with_creator_function) {
 
 TEST_F(BeanTest, test_construct_all_beans_with_creator_function_mixed_with_interface_and_impl) {
     using namespace NormalCase;
-    auto container = ContainerBuilder<>().WithCreators<createCImpl, createA, createB>().Build();
-    auto a = container.GetShared<A>();
-    auto b = container.GetShared<B>();
-    auto c = container.GetShared<C>();
-    auto c1 = container.GetShared<CImpl>();
+    auto factory = FactoryBuilder<>().WithCreators<createCImpl, createA, createB>().Build();
+    auto a = factory.GetShared<A>();
+    auto b = factory.GetShared<B>();
+    auto c = factory.GetShared<C>();
+    auto c1 = factory.GetShared<CImpl>();
     EXPECT_NE(a.get(), nullptr);
     EXPECT_NE(b.get(), nullptr);
     EXPECT_EQ(c.get(), c1.get());
@@ -62,13 +62,13 @@ TEST_F(BeanTest, test_construct_all_beans_with_creator_function_mixed_with_inter
 
 TEST_F(BeanTest, test_construct_beans_unique_ne_shared) {
     using namespace NormalCase;
-    auto container = ContainerBuilder<>().WithBeans<CImpl, BImpl, AImpl>().Build();
-    auto a = container.GetShared<A>();
-    auto b = container.GetShared<B>();
-    auto c = container.GetShared<C>();
-    auto a1 = container.GetUnique<A>();
-    auto b1 = container.GetUnique<B>();
-    auto c1 = container.GetUnique<C>();
+    auto factory = FactoryBuilder<>().WithBeans<CImpl, BImpl, AImpl>().Build();
+    auto a = factory.GetShared<A>();
+    auto b = factory.GetShared<B>();
+    auto c = factory.GetShared<C>();
+    auto a1 = factory.GetUnique<A>();
+    auto b1 = factory.GetUnique<B>();
+    auto c1 = factory.GetUnique<C>();
     EXPECT_NE(a.get(), a1.get());
     EXPECT_NE(b.get(), b1.get());
     EXPECT_NE(c.get(), c1.get());
@@ -76,15 +76,15 @@ TEST_F(BeanTest, test_construct_beans_unique_ne_shared) {
 
 TEST_F(BeanTest, test_construct_beans_with_unique_ptr) {
     using namespace FactoryCreateCase;
-    auto container = ContainerBuilder<>().WithBeans<CImpl, BImpl, AImpl, DImpl>().Build();
-    auto a = container.GetShared<A>();
-    auto b = container.GetShared<B>();
-    auto c = container.GetShared<C>();
-    auto d = container.GetShared<D>();
-    auto a1 = container.GetShared<AImpl>();
-    auto b1 = container.GetShared<BImpl>();
-    auto c1 = container.GetShared<CImpl>();
-    auto d1 = container.GetShared<DImpl>();
+    auto factory = FactoryBuilder<>().WithBeans<CImpl, BImpl, AImpl, DImpl>().Build();
+    auto a = factory.GetShared<A>();
+    auto b = factory.GetShared<B>();
+    auto c = factory.GetShared<C>();
+    auto d = factory.GetShared<D>();
+    auto a1 = factory.GetShared<AImpl>();
+    auto b1 = factory.GetShared<BImpl>();
+    auto c1 = factory.GetShared<CImpl>();
+    auto d1 = factory.GetShared<DImpl>();
     EXPECT_EQ(a.get(), a1.get());
     EXPECT_EQ(b.get(), b1.get());
     EXPECT_EQ(c.get(), c1.get());
@@ -93,11 +93,11 @@ TEST_F(BeanTest, test_construct_beans_with_unique_ptr) {
 
 TEST_F(BeanTest, test_construct_beans_with_list_constructor) {
     using namespace BeansWithListConstructor;
-    auto container = ContainerBuilder<>().WithBeans<CImpl, BImpl, AImpl, B2Impl>().Build();
-    auto c = container.GetShared<C>();
-    auto b = container.GetShared<BImpl>();
-    auto b2 = container.GetShared<B2Impl>();
-    auto c1 = container.GetShared<CImpl>();
+    auto factory = FactoryBuilder<>().WithBeans<CImpl, BImpl, AImpl, B2Impl>().Build();
+    auto c = factory.GetShared<C>();
+    auto b = factory.GetShared<BImpl>();
+    auto b2 = factory.GetShared<B2Impl>();
+    auto c1 = factory.GetShared<CImpl>();
     EXPECT_EQ(c.get(), c1.get());
     EXPECT_TRUE(c1->bs.front().get() == b.get());
     EXPECT_TRUE(c1->bs.back().get() == b2.get());
@@ -105,20 +105,20 @@ TEST_F(BeanTest, test_construct_beans_with_list_constructor) {
 
 TEST_F(BeanTest, test_construct_beans_with_list_constructor_using_getList) {
     using namespace BeansWithListConstructor;
-    auto container = ContainerBuilder<>().WithBeans<CImpl, BImpl, AImpl, B2Impl>().Build();
-    auto bs = container.GetSharedList<B>();
-    auto bs1 = container.GetUniqueList<B>();
+    auto factory = FactoryBuilder<>().WithBeans<CImpl, BImpl, AImpl, B2Impl>().Build();
+    auto bs = factory.GetSharedList<B>();
+    auto bs1 = factory.GetUniqueList<B>();
     EXPECT_EQ(2, bs.size());
     EXPECT_EQ(2, bs1.size());
 }
 
 TEST_F(BeanTest, test_construct_beans_with_list_unique_ptr_constructor) {
     using namespace BeansWithListUniquePtrConstructor;
-    auto container = ContainerBuilder<>().WithBeans<CImpl, BImpl, AImpl, B2Impl>().Build();
-    auto c = container.GetShared<C>();
-    auto b = container.GetShared<BImpl>();
-    auto b2 = container.GetShared<B2Impl>();
-    auto c1 = container.GetShared<CImpl>();
+    auto factory = FactoryBuilder<>().WithBeans<CImpl, BImpl, AImpl, B2Impl>().Build();
+    auto c = factory.GetShared<C>();
+    auto b = factory.GetShared<BImpl>();
+    auto b2 = factory.GetShared<B2Impl>();
+    auto c1 = factory.GetShared<CImpl>();
     EXPECT_EQ(c.get(), c1.get());
     EXPECT_FALSE(std::find_if(c1->bs.begin(), c1->bs.end(), [&](const std::unique_ptr<B>& ptr) {
                      return ptr.get() == b.get();
@@ -130,12 +130,12 @@ TEST_F(BeanTest, test_construct_beans_with_list_unique_ptr_constructor) {
 
 TEST_F(BeanTest, test_construct_beans_with_raw_pointer_constructor) {
     using namespace BeansWithRawPointerConstructor;
-    auto container = ContainerBuilder<>().WithBeans<CImpl, BImpl, AImpl, B2Impl>().Build();
-    auto a = container.GetShared<AImpl>();
-    auto c = container.GetShared<C>();
-    auto b = container.GetShared<BImpl>();
-    auto b2 = container.GetShared<B2Impl>();
-    auto c1 = container.GetShared<CImpl>();
+    auto factory = FactoryBuilder<>().WithBeans<CImpl, BImpl, AImpl, B2Impl>().Build();
+    auto a = factory.GetShared<AImpl>();
+    auto c = factory.GetShared<C>();
+    auto b = factory.GetShared<BImpl>();
+    auto b2 = factory.GetShared<B2Impl>();
+    auto c1 = factory.GetShared<CImpl>();
     EXPECT_EQ(c.get(), c1.get());
     EXPECT_TRUE(c1->bs.front() == b.get());
     EXPECT_TRUE(c1->bs.back() == b2.get());
@@ -144,31 +144,31 @@ TEST_F(BeanTest, test_construct_beans_with_raw_pointer_constructor) {
 
 TEST_F(BeanTest, test_construct_beans_with_unique_but_has_nested_shared_ptr_constructor) {
     using namespace BeansWithUniqueButHasSharedPtrConstructor;
-    auto container = ContainerBuilder<>().WithBeans<CImpl, BImpl, AImpl>().Build();
-    auto a = container.GetShared<AImpl>();
-    auto c = container.GetShared<C>();
-    auto b = container.GetShared<BImpl>();
-    auto c1 = container.GetShared<CImpl>();
+    auto factory = FactoryBuilder<>().WithBeans<CImpl, BImpl, AImpl>().Build();
+    auto a = factory.GetShared<AImpl>();
+    auto c = factory.GetShared<C>();
+    auto b = factory.GetShared<BImpl>();
+    auto c1 = factory.GetShared<CImpl>();
     EXPECT_EQ(c.get(), c1.get());
     EXPECT_NE(b.get(), c1->b.get());
 }
 
 TEST_F(BeanTest, test_construct_beans_without_interface_heritated) {
     using namespace BeansWithoutInterfaceHeritated;
-    auto container = ContainerBuilder<>().WithBeans<CImpl, BImpl, AImpl>().Build();
-    auto a = container.GetShared<AImpl>();
-    auto b = container.GetShared<BImpl>();
-    auto c = container.GetShared<CImpl>();
+    auto factory = FactoryBuilder<>().WithBeans<CImpl, BImpl, AImpl>().Build();
+    auto a = factory.GetShared<AImpl>();
+    auto b = factory.GetShared<BImpl>();
+    auto c = factory.GetShared<CImpl>();
     EXPECT_NE(b.get(), c->b.get());
     EXPECT_EQ(a.get(), b->a.get());
 }
 
 TEST_F(BeanTest, test_construct_beans_with_no_macro) {
     using namespace BeanWithNoMacro;
-    auto container = ContainerBuilder<>().WithBeans<CImpl, BImpl, A>().Build();
-    auto a = container.GetShared<A>();
-    auto b = container.GetShared<BImpl>();
-    auto c = container.GetShared<CImpl>();
+    auto factory = FactoryBuilder<>().WithBeans<CImpl, BImpl, A>().Build();
+    auto a = factory.GetShared<A>();
+    auto b = factory.GetShared<BImpl>();
+    auto c = factory.GetShared<CImpl>();
     EXPECT_NE(a, nullptr);
     EXPECT_NE(b, nullptr);
     EXPECT_NE(c, nullptr);
